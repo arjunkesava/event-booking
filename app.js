ee821a224995fe0f6ef97e84db13b69d35103ee1
arjunkesava <arjun.kesava@gmail.com>
2019-02-15 13:14:40 +0530

    Connected Graphql Backend and React FrontEnd both

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const graphqlResolvers = require('./graphql/resolvers/index')
 
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Methods','POST,GET,OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization')
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200)
+    }
+    next()
+})
+
 app.use(isAuth)
 
 app.use(
@@ -26,7 +36,7 @@ mongoose
     .connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ds121295.mlab.com:21295/event_booking`)
     .then(() => {
         console.log('Mongo connected, go ahead')
-        app.listen(5000)
+        app.listen(9000)
     })
     .catch(err => {
         throw err
